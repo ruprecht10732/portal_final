7e498231afa73727a0f732583abf2103bb3b33cd
oostrobin <153209318+oostrobin@users.noreply.github.com>
2026-02-04 18:56:53 +0100

    feat(catalog): enhance materials card layout with improved styling and structure

--- src/app/routes/catalog/catalog-detail/catalog-detail-materials-card/catalog-detail-materials-card.component.ts
+++ src/app/routes/catalog/catalog-detail/catalog-detail-materials-card/catalog-detail-materials-card.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { type Product } from '../../../../core/services/catalog.service';
+import { ChipComponent } from '../../../../shared/components/chip/chip.component';
 
 @Component({
   selector: 'app-catalog-detail-materials-card',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ChipComponent],
   templateUrl: './catalog-detail-materials-card.component.html',
   styleUrl: './catalog-detail-materials-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
