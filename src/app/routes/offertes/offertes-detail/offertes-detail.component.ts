1b0fb6daafc4d7c00272be0c15774ca0e396635b
ruprecht10732 <oostrobin1989@gmail.com>
2026-04-02 10:52:46 +0200

    feat(offertes-detail): update icon for mark sent action in offerte detail

--- src/app/routes/offertes/offertes-detail/offertes-detail.component.ts
+++ src/app/routes/offertes/offertes-detail/offertes-detail.component.ts
@@ -229,7 +229,7 @@ export class OffertesDetailComponent implements OnInit {
             {
               label: 'offertes.markSent',
               action: 'markSent',
-              icon: 'badge-check',
+              icon: 'send',
               disabled: this.updating(),
             },
           ]
